2367fe0b6d88261d812d37276e511f5c9bf8d840
arpitr10x <arpit@retail10x.com>
2025-12-23 16:54:25 +0530

    change

--- main.py
+++ main.py
@@ -51,6 +51,6 @@ def note_create():
 #app.run(debug=True)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    #app.run(host="0.0.0.0", port=port)
     #app.run(debug=True)
 
